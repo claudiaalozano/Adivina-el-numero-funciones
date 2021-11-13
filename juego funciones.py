15b558b6a1f7536bbde0ac50750c1cd2f43d43de
claudiaalozano <clau.lozano.perez@gmail.com>
2021-11-13 16:36:39 +0100

    commit 2

--- juego funciones.py
+++ juego funciones.py
@@ -1,19 +1,23 @@
 import random
 while True:
     nivel=input("Seleccione un nivel entre fácil, medio, difícil o legendario: ")
-        if nivel=="fácil":
-            MIN: 0
-            MAX: 99
-            break
-        if nivel=="medio":
-            MIN: 0
-            MAX: 999
-            break
-        if nivel=="difícil":
-            MIN: 0
-            MAX: 9999
-            break
-        if nivel=="legendario":
-            MIN: 0
-            MAX: 99999
-            break
+    if nivel=="fácil":
+        MIN: 0
+        MAX: 99
+        break
+    if nivel=="medio":
+        MIN: 0
+        MAX: 999
+        break
+    if nivel=="difícil":
+        MIN: 0
+        MAX: 9999
+        break
+    if nivel=="legendario":
+        MIN: 0
+        MAX: 99999
+        break
+def numero (minimo = MIN , maximo = MAX):
+    while True:
+        numero2= int(input("Por favor seleccione un número: "))
+        
