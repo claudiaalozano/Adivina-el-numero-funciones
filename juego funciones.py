f8a93599b494e5b90611c608c9d9c50ecc041e2f
claudiaalozano <clau.lozano.perez@gmail.com>
2021-11-13 17:24:04 +0100

    commit 3

--- juego funciones.py
+++ juego funciones.py
@@ -1,23 +1,36 @@
 import random
+MIN= 0
+MAX= 0
 while True:
     nivel=input("Seleccione un nivel entre fácil, medio, difícil o legendario: ")
     if nivel=="fácil":
-        MIN: 0
-        MAX: 99
+        MIN= 0
+        MAX= 99
         break
     if nivel=="medio":
-        MIN: 0
-        MAX: 999
+        MIN= 0
+        MAX= 999
         break
     if nivel=="difícil":
-        MIN: 0
-        MAX: 9999
+        MIN= 0
+        MAX= 9999
         break
     if nivel=="legendario":
-        MIN: 0
-        MAX: 99999
+        MIN= 0
+        MAX= 99999
         break
-def numero (minimo = MIN , maximo = MAX):
+numero=random.randint(MIN,MAX)
+
+def numero_elegido (invitación):
     while True:
         numero2= int(input("Por favor seleccione un número: "))
-        
+        if minimo<=numero2<=maximo:
+            minimo=minimo + 1
+            maximo=maximo - 1
+    while numero != numero2:
+        if numero < numero2:
+            print("El número introducido es grande, vuelva a intentarlo.")
+        if numero > numero2:
+            print("El número introducido es pequeño, vuelva a intentarlo.")
+        if numero == numero2:
+            print("Ha ganado el juego.")
