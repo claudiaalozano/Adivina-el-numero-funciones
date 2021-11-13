3df37c7d2f9854d1e4d27aabbd119dde04be348d
claudiaalozano <clau.lozano.perez@gmail.com>
2021-11-13 16:05:53 +0100

    cambios 2

--- juego funciones.py
+++ juego funciones.py
@@ -1,4 +1,14 @@
-import random
-minimo= 0
-maximo= 99
-numero= int(input ("Introduzca un número: "))
+nivel= int(input("Por favor selecciona un nivel entre fácil, medio, díficil y legendario: "))
+while nivel==facil:
+    MIN= 0
+    MAX= 99
+    def numero 
+ while nivel==medio:
+     MIN: 0
+     MAX: 999
+while nivel==dificil:
+    MIN: 0
+    MAX: 9999
+while nivel==legendario:
+    MIN: 0
+    MAX: 999999
